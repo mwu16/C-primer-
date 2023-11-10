563b8be9829b1d43382efed5d95275c9d7283814
mwu16 <min.wu@volvocars.com>
2023-11-10 10:53:00 +0100

    modify the code to runable

---
+++ Chapter 3/3_18.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    std::vector<int> ivec;
+    ivec.push_back(42);
+
+    std::cout << ivec[0] << std::endl;
+    return 0;
+}
