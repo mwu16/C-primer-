728083036ea044e0af9ea227dfe43fd3946ba1c7
mwu16 <min.wu@volvocars.com>
2023-12-06 11:33:10 +0100

    create 10 interger list and transfer to vector

---
+++ Chapter 3/3_41.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+int main() {
+    const int sz=8;
+    int list1[10];
+    std::srand((unsigned) time(NULL));
+    for (int i=0; i<sz; i++){
+        list1[i]=std::rand()/100; //int 0 to 10
+    }
+    std::vector<int> ivec(std::begin(list1), std::end(list1));
+    for (decltype(ivec.size()) i=0; i<ivec.size(); i++){
+        std::cout << ivec[i] << std::endl;
+    }
+    std::cout<<std::endl;
+    return 0;
+}
